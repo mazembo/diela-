207ec4a34a2330b81803b1dee95f70b92d5fb404
Mazembo <mazemb_eddy@yahoo.fr>
2010-11-13 00:51:14 +0200

    modifying env

--- config/environment.rb
+++ config/environment.rb
@@ -28,8 +28,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
     config.gem "RedCloth"
-    config.gem 'ryanb-acts-as-list', :lib => 'acts_as_list', :source => 'http://gems.github.com'
-    config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  #  config.gem 'ryanb-acts-as-list', :lib => 'acts_as_list', :source => 'http://gems.github.com'
+  # config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
     
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
